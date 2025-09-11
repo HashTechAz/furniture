0c8a506b52ae21074c8912a05d762c382002e5a5
Turan Hasanov <turanhasan013@gmail.com>
2025-09-11 10:40:07 +0400

    Add Palette component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import styles from './page.module.css';   
 import HeroSection from '@/components/HeroSection/HeroSection';
 import ProductSlider from '@/components/ProductSlider/ProductSlider';
+import Palette from '@/components/Palette/Palette';
 
 export default function Home() {
   return (
     <>
       <HeroSection/>
       <ProductSlider/>
+      <Palette/>
     </>
   );
 }
---
+++ src/components/Palette/Palette.tsx
@@ -0,0 +1,34 @@
+import styles from "./Palette.module.css";
+
+export default function Palette() {
+  return (
+    <>
+      <section className={styles.paletteMain}>
+        <div className={styles.paletteTextBox}>
+          <h1 className={styles.paletteTextBoxTitle}>
+            <span>News</span>
+          </h1>
+          <h2 className={styles.paletteTextBoxTitle}>
+            <span>Refreshing the Palette</span>
+          </h2>
+          <div className={styles.paletteDescription}>
+            <p>
+              The Montana Mini series is now even more versatile with the
+              introduction of an updated colour range. Whether you’re looking to
+              add warmth, make a bold statement, or create a subtle touch of
+              elegance, the new colours empower you to design a space that feels
+              uniquely yours.
+            </p>
+          </div>
+          <div>
+            <a href="#" className={styles.heroButton}>
+              Explore now
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.paletteImgBox}></div>
+      </section>
+    </>
+  );
+}
