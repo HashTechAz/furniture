444ca38a8addf98306653d27ed410fff927ef613
turanlz <turanhasan013@gmail.com>
2025-09-13 05:12:41 +0400

    feat: Update HomeVideo component with image display and CSS enhancements
    This commit modifies the HomeVideo component to include an image, improving visual engagement. CSS styles are updated to adjust the layout, including changes to height, padding, and flex properties for better content alignment and responsiveness.

--- src/components/HomeVideo/HomeVideo.tsx
+++ src/components/HomeVideo/HomeVideo.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import styles from "./HomeVideo.module.css"
+import React from "react";
+import styles from "./HomeVideo.module.css";
 
 const HomeVideo = () => {
   return (
     <>
-     <section className={styles.videoMain}>
-        <div className={styles.videoContent}></div>
-     </section>
+      <section className={styles.videoMain}>
+        <div className={styles.videoContent}>
+        
+          <img
+            src='https://b2c.montana-episerver.com/globalassets/ambient-images/landscape-images/fairs-and-showrooms/skindergade-showroom/montana_showroom_skindergade_09_w.jpg?mode=crop&width=1520&height=1093'
+            alt='Montana'
+          />
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default HomeVideo
+export default HomeVideo;
