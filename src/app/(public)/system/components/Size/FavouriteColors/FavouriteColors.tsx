53d9b24c0a2861b2e8d3084c86b3f5b41285c646
Turan Hasanov <turanhasan013@gmail.com>
2025-09-16 17:54:17 +0400

    feat: Enhance FavouriteColors component with new text and button styles
    This commit updates the FavouriteColors component by adding descriptive text paragraphs to improve user engagement. Additionally, new styles are introduced for the button, enhancing its appearance and interactivity. The layout is also adjusted for better visual alignment.

--- src/app/(public)/system/components/Size/FavouriteColors/FavouriteColors.tsx
+++ src/app/(public)/system/components/Size/FavouriteColors/FavouriteColors.tsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import styles from "./FavouriteColors.module.css"
+import React from "react";
+import styles from "./FavouriteColors.module.css";
+import Link from "next/link";
 
 const FavouriteColors = () => {
   return (
     <section className={styles.fav}>
-        <div className={styles.favMain}>
-            <h2>3. Choose your favourite colours</h2>
-        </div>
-    </section>
-  )
-}
+      <div className={styles.favMain}>
+        <h2>3. Choose your favourite colours</h2>
 
-export default FavouriteColors
+        <p className={styles.favText}>
+          Montana’s functional and flexible system is featured in a range of 43
+          poetic and complex colours developed in close collaboration with the
+          award-winning Danish designer and colour expert Margrethe Odgaard.
+        </p>
+
+        <p className={styles.favText}>
+          Colours mean everything. Ambience. Atmosphere. Identity. Colours are
+          paramount in our design. We want to influence and inspire the world of
+          interiors with our take on colours. Bright and light. Dense and deep.
+          There is a colour for any purpose.
+        </p>
+
+        <div className={styles.favBtn}>
+          <Link href={"/"}>Explore the colours</Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FavouriteColors;
