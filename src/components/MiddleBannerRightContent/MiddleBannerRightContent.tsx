a5c600f167124fcce604c22f9c26e13a6983ad66
turanlz <turanhasan013@gmail.com>
2025-10-04 13:54:52 +0400

    feat(CreativeMinds): Refine layout and styles for Lumikello page components
    This commit enhances the layout and styles of the Lumikello page within the Creative Minds section. It introduces adjustments to the CSS for better spacing and visual consistency, particularly in the hero section and header. Additionally, it updates the rendering logic to improve component integration, ensuring a more cohesive user experience across the application.

---
+++ src/components/MiddleBannerRightContent/MiddleBannerRightContent.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import styles from "./MiddleBannerRightContent.module.css";
+
+interface MiddleBannerProps {
+  largeImageUrl: string;
+  smallImageUrl: string;
+  title: string;
+  description: string;
+  buttonText: string;
+  buttonLink: string;
+  layout?: 'imageLeft' | 'imageRight';
+  smallImageHeight?: string;
+}
+
+const MiddleBanner = ({
+  largeImageUrl,
+  smallImageUrl,
+  title,
+  description,
+  buttonText,
+  buttonLink,
+  layout = 'imageLeft',
+  smallImageHeight,
+}: MiddleBannerProps) => {
+
+  const LargeImageBlock = (
+    <div className={styles.leftBanner}>
+      <img src={largeImageUrl} alt={title} />
+    </div>
+  );
+
+  const TextAndSmallImageBlock = (
+    <div className={styles.rightBanner}>
+      <div className={styles.rightImgContainer} style={{ height: smallImageHeight }}>
+        <img src={smallImageUrl} alt={title} className={styles.rightImg} />
+      </div>
+      <div className={styles.rightText}>
+        <h1 className={styles.title}>{title}</h1>
+        <p>{description}</p>
+        <div>
+          <a href={buttonLink} className={styles.heroButton}>
+            {buttonText}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <section className={`${styles.mBanner} ${layout === 'imageRight' ? styles.reversed : ''}`}>
+      {layout === 'imageLeft' ? (
+        <>
+          {LargeImageBlock}
+          {TextAndSmallImageBlock}
+        </>
+      ) : (
+        <>
+          {TextAndSmallImageBlock}
+          {LargeImageBlock}
+        </>
+      )}
+    </section>
+  );
+};
+
+export default MiddleBanner;
