ed59566b728eb22a0c064e17bf1056cbc240d4b1
Turan Hasanov <turanhasan013@gmail.com>
2025-10-28 09:32:43 +0400

    feat(HomePage): Refactor page.tsx and NewsSection to improve code clarity and responsiveness
    This commit refines the Home component by standardizing quotation marks and enhancing the layout of various sections, including HeroSection and PaletteRightImage components. Additionally, the NewsSection component is updated to import news data from a JSON file, replacing hardcoded data, and modifies the news title for better context. These changes aim to improve code maintainability and enhance the overall user experience.

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -33,65 +33,65 @@ const bannerDataReversed = {
   buttonLink: "/sofas",
   layout: "imageRight",
   smallImageHeight: "400px",
-  textBlockWidth: "40%", 
+  textBlockWidth: "40%",
 } as const;
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      <div id='header-trigger' style={{ height: 1 }} />
+      <div id="header-trigger" style={{ height: 1 }} />
       <ProductSlider />
       <PaletteRightImage
-        title='Refreshing the Palette'
+        title="Refreshing the Palette"
         description="The Montana Mini series is now even more versatile with the introduction of an updated colour range. Whether you're looking to add warmth, make a bold statement, or create a subtle touch of elegance, the new colours empower you to design a space that feels uniquely yours."
-        buttonText='Explore now'
-        buttonLink='/news/palette-refresh'
-        imageUrl='https://b2c.montana-episerver.com/globalassets/ambient-images/portrait-images/montana-mini/ss25/montana_mini_displayshelf_rosehip_flint_amber_ruby_clay_h.jpg?mode=crop&width=540&height=720'
-        backgroundColor='#CCC6B8'
+        buttonText="Explore now"
+        buttonLink="/news/palette-refresh"
+        imageUrl="https://b2c.montana-episerver.com/globalassets/ambient-images/portrait-images/montana-mini/ss25/montana_mini_displayshelf_rosehip_flint_amber_ruby_clay_h.jpg?mode=crop&width=540&height=720"
+        backgroundColor="#CCC6B8"
       />
       <NewsSection limit={4} />
       <ProductNewsSlider />
-      <div className='hideOnMobile'>
+      <div className="hideOnMobile">
         <MiddleBanner {...bannerDataDefault} />
       </div>
-      <div className='showOnlyOnMobile'>
-        <HomeVideo variant='mobile' />
+      <div className="showOnlyOnMobile">
+        <HomeVideo variant="mobile" />
       </div>
       <TrustBadges />
       <PaletteRightImage
-        title='Colour Class – Learn to embrace the world of colours'
-        description='Montana Furniture has teamed up with Color Connaisseur by Céline Hallas to create Colour Class, a unique digital concept designed to empower you to confidently embrace the world of colours within your interior spaces. In each Colour Class, we invite you into homes that embrace vibrant colour schemes, showing how you can use colours to reflect your personality and express your unique style in interior design.'
-        buttonText='Discover Colour Classes here'
-        buttonLink='/collections/summer'
-        imageUrl='https://b2c.montana-episerver.com/globalassets/ambient-images/portrait-images/colours/colour-class/cc-02/Montana_Colour_Class_2_header_iris_2024.jpg?mode=crop&width=828&height=595'
-        backgroundColor='#BDD2DA'
-        imageSize='large'
+        title="Colour Class – Learn to embrace the world of colours"
+        description="Montana Furniture has teamed up with Color Connaisseur by Céline Hallas to create Colour Class, a unique digital concept designed to empower you to confidently embrace the world of colours within your interior spaces. In each Colour Class, we invite you into homes that embrace vibrant colour schemes, showing how you can use colours to reflect your personality and express your unique style in interior design."
+        buttonText="Discover Colour Classes here"
+        buttonLink="/collections/summer"
+        imageUrl="https://b2c.montana-episerver.com/globalassets/ambient-images/portrait-images/colours/colour-class/cc-02/Montana_Colour_Class_2_header_iris_2024.jpg?mode=crop&width=828&height=595"
+        backgroundColor="#BDD2DA"
+        imageSize="large"
       />
 
       <MiddleBanner {...bannerDataReversed} />
       <PaletteLeftImage
-        title='Creating good design demands honesty and respect'
-        description='Montana Furniture is a family-owned company, established in 1982, leading within storage and furniture for private homes and contemporary office spaces. The company is founded by Peter J. Lassen, who is also the designer of the Montana system.
+        title="Creating good design demands honesty and respect"
+        description="Montana Furniture is a family-owned company, established in 1982, leading within storage and furniture for private homes and contemporary office spaces. The company is founded by Peter J. Lassen, who is also the designer of the Montana system.
 
-All Montana modules are designed, developed and made in Denmark. Every day, in a small town on the island of Funen over 140 professionals work hard to uphold the highest standards of processing, painting and assembling – making sure that your Montana furniture will last a lifetime.'
-        buttonText='Discover Colour Classes here'
-        buttonLink='/collections/summer'
-        imageUrl='https://b2c.montana-episerver.com/globalassets/ambient-images/square-images/designer-portraits/montana_peter_joakim_lassen_01.jpg?mode=crop&width=828&height=595'
-        backgroundColor='#2C3587'
-        imageSize='large'
+All Montana modules are designed, developed and made in Denmark. Every day, in a small town on the island of Funen over 140 professionals work hard to uphold the highest standards of processing, painting and assembling – making sure that your Montana furniture will last a lifetime."
+        buttonText="Discover Colour Classes here"
+        buttonLink="/collections/summer"
+        imageUrl="https://b2c.montana-episerver.com/globalassets/ambient-images/square-images/designer-portraits/montana_peter_joakim_lassen_01.jpg?mode=crop&width=828&height=595"
+        backgroundColor="#2C3587"
+        imageSize="large"
       />
-      <div className='hideOnMobile'>
+      <div className="hideOnMobile">
         <HomeVideo />
       </div>
       <PaletteRightImage
-        title='Montana for professionals'
-        description='Visit another part of our website designed for professionals within interior design. At Montana we are passionate about helping architects, designers, buyers and project managers develop functional and aesthetic professional environments. Find our sales team, product information and other resources.'
-        buttonText='Montana for professionals'
-        buttonLink='/collections/summer'
-        imageUrl='https://b2c.montana-episerver.com/globalassets/ambient-images/landscape-images/montana-office/2022/montana_office_canteen_breakoutarea_mosertable_kevi_12mmsystem_h_crop.jpg?mode=crop&width=828&height=595'
-        backgroundColor='#263835'
-        imageSize='large'
+        title="Montana for professionals"
+        description="Visit another part of our website designed for professionals within interior design. At Montana we are passionate about helping architects, designers, buyers and project managers develop functional and aesthetic professional environments. Find our sales team, product information and other resources."
+        buttonText="Montana for professionals"
+        buttonLink="/collections/summer"
+        imageUrl="https://b2c.montana-episerver.com/globalassets/ambient-images/landscape-images/montana-office/2022/montana_office_canteen_breakoutarea_mosertable_kevi_12mmsystem_h_crop.jpg?mode=crop&width=828&height=595"
+        backgroundColor="#263835"
+        imageSize="large"
       />
       <Form />
     </>
