45a0e09e87cd699e41c8f0ecf4d1c2ff31e09a80
Anvar <aliyev612@gmail.com>
2026-02-25 13:48:42 +0400

    mock

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -1,9 +1,8 @@
 import { NextResponse } from 'next/server';
-import newsData from '../../../mock/newsData.json';
 
 export async function GET() {
   try {
-    return NextResponse.json(newsData);
+    return NextResponse.json([]);
   } catch (error) {
     console.error('Failed to fetch news data:', error);
     return NextResponse.json(
